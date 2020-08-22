eb2a31c68366c82c872742f7acdd163b65a6e16b
kkwkkj@naver.com <kkwkkj@naver.com>
2020-08-22 17:39:23 +0900

    index page

--- Mypet_Project/src/main/java/com/mypet/controller/AlbumController.java
+++ Mypet_Project/src/main/java/com/mypet/controller/AlbumController.java
@@ -1,5 +1,57 @@
 package com.mypet.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mypet.domain.AlbumVO;
+import com.mypet.service.AlbumService;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/album/*")
+@AllArgsConstructor
 public class AlbumController {
 
+	private AlbumService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		model.addAttribute("list",service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(AlbumVO album, RedirectAttributes rttr) {
+		service.register(album);
+		rttr.addFlashAttribute("result", album.getA_no());
+		return "redirect:/album/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("a_no")int a_no, Model model) {
+		model.addAttribute("album",service.get(a_no));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(AlbumVO album, RedirectAttributes rttr) {
+		
+		if(service.modify(album)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/album/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("a_no")int a_no, RedirectAttributes rttr) {
+		if(service.remove(a_no)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/album/list";
+	}
+	
 }
