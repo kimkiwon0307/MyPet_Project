9590969e613e0d95137d6c1b06a60c8f34a0bc78
kkwkkj@naver.com <kkwkkj@naver.com>
2020-09-12 20:20:48 +0900

    freecontroller

--- Mypet_Project/src/main/java/com/mypet/controller/FreeBoardContoller.java
+++ Mypet_Project/src/main/java/com/mypet/controller/FreeBoardContoller.java
@@ -3,8 +3,11 @@ package com.mypet.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mypet.service.FreeBoardService;
+
 @Controller
 @RequestMapping("/free/*")
 public class FreeBoardContoller {
-
+	
+	private FreeBoardService service;
 }
