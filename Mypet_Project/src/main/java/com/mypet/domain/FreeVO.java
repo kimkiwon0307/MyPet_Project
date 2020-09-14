c351ecff4da1c2262ec835b53f0427601203dae5
kkwkkj@naver.com <kkwkkj@naver.com>
2020-09-14 14:11:24 +0900

    에러 수정

--- Mypet_Project/src/main/java/com/mypet/domain/FreeVO.java
+++ Mypet_Project/src/main/java/com/mypet/domain/FreeVO.java
@@ -9,7 +9,8 @@ public class FreeVO {
 	
 	private int f_no;
 	private String f_title;
-	private String f_countent;
+	private String f_content;
+	private String f_writer;
 	private String f_photo;
 	private Date f_date;
 	private Date f_udate;
