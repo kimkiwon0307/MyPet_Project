f517fbb2d1f012da430a978144ce42a3ca839a03
kkwkkj@naver.com <kkwkkj@naver.com>
2021-04-05 17:15:06 +0900

    자유게시판 댓글-동기식 하는중

--- Mypet_Project/src/main/java/com/mypet/mapper/FreeBoardMapper.java
+++ Mypet_Project/src/main/java/com/mypet/mapper/FreeBoardMapper.java
@@ -2,11 +2,14 @@ package com.mypet.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.mypet.domain.Criteria;
 import com.mypet.domain.FreeBoardVO;
 
 public interface FreeBoardMapper {
 
+	
 	public List<FreeBoardVO> getList();      // 리스트
 	
 	public void insert(FreeBoardVO free);    // C   
